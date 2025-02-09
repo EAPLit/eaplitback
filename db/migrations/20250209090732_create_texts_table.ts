9a20dee1fb2490769d59e63f0052336605d38a03
DanielWilliamParsons <parsonsdwp@gmail.com>
2025-02-09 18:10:49 +0900

    build: add texts table for users to upload texts

---
+++ db/migrations/20250209090732_create_texts_table.ts
@@ -0,0 +1,22 @@
+import type { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable("texts", (table) => {
+        table.uuid("textID").primary();
+        table
+            .uuid("userID")
+            .notNullable()
+            .references("id")
+            .inTable("users")
+            .onDelete("CASCADE")
+        table.text("text").notNullable();
+        table.timestamps(true, true);
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTableIfExists("texts");
+}
+
