bbdf04464baa5b23959167da5a32d92392df34b5
DanielWilliamParsons <parsonsdwp@gmail.com>
2025-02-11 11:39:54 +0900

    feat: add route and model to start a new project

--- src/router/mylearning.ts
+++ src/router/mylearning.ts
@@ -8,4 +8,9 @@ mylearningRouter.get('/projectList', (req: Request, res: Response) => {
     res.json({ message: "success" });
 });
 
+mylearningRouter.post('/project', (req: Request, res: Response) => {
+
+    res.json({ message: "success" });
+});
+
 export default mylearningRouter;
